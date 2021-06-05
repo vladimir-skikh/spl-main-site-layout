af993ec91045ffdcf7b78047f78347224b2f5c3f
vladimir-sikkh <19vladimir99@mail.ru>
2021-06-05 15:37:02 +0300

    добавил видео на страницу О Нас

---
+++ src/js/aboutus-page.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let video_play_button = document.querySelector('.video__button');
+
+    video_play_button.addEventListener('click', playVideo, false);
+    video_play_button.addEventListener('onkeyup', playVideo, false);
+});
+
+const playVideo = (evt) => {
+    let video = document.querySelector('.video__source');
+    let video_play_cover = document.querySelector('.video__cover');
+
+    evt.currentTarget.classList.add('video__button_hidden')
+    video_play_cover.classList.add('video__cover_hidden');
+
+    video.classList.add('video__source_visible');
+    video.play();
+}
